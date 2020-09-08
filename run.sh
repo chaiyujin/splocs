cf44949e56ab7df0ebee6aa548ae74b70d076f36
Yuki-Chai <504137683@qq.com>
2020-09-08 13:30:58 +0800

    ..

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python3 import_ply.py "/media/yuki/65F33762C14D581B/Datasets/vocaset_avg/*.ply" ./data/voca_0.h5
-python3 align_rigid.py ./data/voca_0.h5 ./data/voca_0_aligned.h5
-python3 sploc.py ./data/voca_0_aligned.h5  ./data/voca_0_splocs.h5
-python3 view_splocs.py ./data/voca_0_splocs.h5
+# python3 import_ply.py "/media/yuki/65F33762C14D581B/Datasets/vocaset_avg_unposed/*.ply" ./data/voca_face5.h5
+# python3 align_rigid.py ./data/voca_face5.h5 ./data/voca_face5_aligned.h5
+python3 sploc.py ./data/voca_face5_aligned.h5  ./data/voca_face5_splocs.h5
+python3 view_splocs.py ./data/voca_face5_splocs.h5
