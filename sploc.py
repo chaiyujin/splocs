739fb063b21378ce92985d7cf33f1444474b5829
yuki-chai <chaiyujin@gmail.com>
2021-01-11 17:48:48 +0800

    Update sploc.py

--- sploc.py
+++ sploc.py
@@ -27,11 +27,11 @@ def compute_support_map(idx, geodesics, min_dist, max_dist):
 
 def main(input_animation_file, output_sploc_file, output_animation_file):
     rest_shape = "first"  # which frame to use as rest-shape ("first" or "average")
-    K = 25  # number of components
+    K = 50  # number of components
     smooth_min_dist = 0.01  # minimum geodesic distance for support map, d_min_in paper
-    smooth_max_dist = 0.20  # maximum geodesic distance for support map, d_max in paper
+    smooth_max_dist = 0.15  # maximum geodesic distance for support map, d_max in paper
     num_iters_max   = 20  # number of iterations to run
-    sparsity_lambda = 10.  # sparsity parameter, lambda in the paper
+    sparsity_lambda = 40.  # sparsity parameter, lambda in the paper
 
     rho = 10.0  # penalty parameter for ADMM
     num_admm_iterations = 10  # number of ADMM iterations
